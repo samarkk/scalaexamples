8c2c7bbb2b9ce725c6a03a4c0934b3f2eece56ab
The Learner <samarkant1@yahoo.com>
2018-04-26 11:21:18 +0530

    Update ScalaExamples.scala

--- src/main/scala/com/skk/training/ScalaExamples.scala
+++ src/main/scala/com/skk/training/ScalaExamples.scala
@@ -1,1 +1,18 @@
-
+object ScalaExamples extends App{
+  /*
+  1. Implement a compose method with the following signature:
+def compose[A, B, C]
+(g: B => C, f: A => B): A => C = ???
+This method must return a function h, which is the composition of the
+functions f and g
+*/
+ def compose[A, B, C](g: B => C, f: A => B): A => C = (x:A) => g(f(x))
+  /*
+  Implement a fuse method with the following signature:
+def fuse[A, B]
+(a: Option[A], b: Option[B]): Option[(A, B)] = ???
+The resulting Option object should contain a tuple of values from the
+Option objects a and b, given that both a and b are non-empty. Use forcomprehensions
+  */
+ def fuse[A, B](a: Option[A], b: Option[B]): Option[(A, B)] = Option((a.get, b.get))
+}
